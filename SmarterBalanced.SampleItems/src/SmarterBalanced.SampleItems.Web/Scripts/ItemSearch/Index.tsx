86aeebc5166439c0c51ddc259038d49feeb4bd21
millcour <courtney.miller005@gmail.com>
2017-09-28 16:00:41 -0700

    added index file for entry point

---
+++ SmarterBalanced.SampleItems/src/SmarterBalanced.SampleItems.Web/Scripts/ItemSearch/Index.tsx
@@ -0,0 +1,16 @@
+﻿import * as ReactDOM from 'react-dom';
+import * as React from 'react';
+import * as ItemsSearch from './ItemsSearch';
+import * as ItemSearchModels from './ItemsSearchModels';
+import { get } from "../ApiModel";
+import * as ItemCardModels from "../ItemCard/ItemCardModels"
+
+
+export function initializeItemsSearch(viewModel: ItemSearchModels.ItemsSearchViewModel) {
+    const searchClient = () => get<ItemCardModels.ItemCardViewModel[]>("/BrowseItems/search");
+    const props = { ...viewModel, itemsSearchClient: searchClient };
+
+    ReactDOM.render(
+        <ItemsSearch.ItemsSearch {...props} />,
+        document.getElementById("search-container") as HTMLElement);
+}
