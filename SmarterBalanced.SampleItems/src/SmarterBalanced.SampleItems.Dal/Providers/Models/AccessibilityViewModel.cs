38b61219b66734f5850c7a196a71c0bf6a8392ac
millcour <courtney.miller005@gmail.com>
2017-10-24 14:46:36 -0700

    added AccessibilityViewModel to separate accessibility from AboutThisItemViewModel

---
+++ SmarterBalanced.SampleItems/src/SmarterBalanced.SampleItems.Dal/Providers/Models/AccessibilityViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmarterBalanced.SampleItems.Dal.Providers.Models
+{
+    public class AccessibilityViewModel
+    {
+    }
+}
