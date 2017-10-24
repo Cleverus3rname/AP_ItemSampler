9e7b653f018b8c9f6942286e146e3ef3b408073b
millcour <courtney.miller005@gmail.com>
2017-10-24 15:03:26 -0700

    added fields to the AccessibilityViewModel

--- SmarterBalanced.SampleItems/src/SmarterBalanced.SampleItems.Dal/Providers/Models/AccessibilityViewModel.cs
+++ SmarterBalanced.SampleItems/src/SmarterBalanced.SampleItems.Dal/Providers/Models/AccessibilityViewModel.cs
@@ -1,10 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Collections.Immutable;
 
 namespace SmarterBalanced.SampleItems.Dal.Providers.Models
 {
     public class AccessibilityViewModel
     {
+        public ImmutableArray<AccessibilityFamily> Families { get; }
+        public ImmutableArray<AccessibilityResource> Resources { get; }
+        public ImmutableArray<AccessibilitySelection> Selections { get; }
+
+        public AccessibilityViewModel(
+            ImmutableArray<AccessibilityFamily> families,
+            ImmutableArray<AccessibilityResource> resources,
+            ImmutableArray<AccessibilitySelection> selections)
+        {
+            Families = families;
+            Resources = resources;
+            Selections = selections;
+        }
+
+        public static AccessibilityViewModel Create(
+           ImmutableArray<AccessibilityFamily> families = new ImmutableArray<AccessibilityFamily>(),
+           ImmutableArray<AccessibilityResource> resources = new ImmutableArray<AccessibilityResource>(),
+           ImmutableArray<AccessibilitySelection> selections = new ImmutableArray<AccessibilitySelection>())
+        {
+            return new AccessibilityViewModel(
+                families: families,
+                resources: resources,
+                selections: selections
+            );
+        }
     }
 }
