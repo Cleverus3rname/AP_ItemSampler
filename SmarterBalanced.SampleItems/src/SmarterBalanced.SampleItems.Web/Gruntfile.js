adcc8ba5a398dd28989d62d88d1ef70109809d57
Alex Lepinski <alexjlepinski@gmail.com>
2017-11-03 23:33:12 -0700

    Removed grunt less,webpack, and min. added less loader to webpack

--- SmarterBalanced.SampleItems/src/SmarterBalanced.SampleItems.Web/Gruntfile.js
+++ SmarterBalanced.SampleItems/src/SmarterBalanced.SampleItems.Web/Gruntfile.js
@@ -1,40 +1,9 @@
 /// <binding ProjectOpened='all, watch' />
 'use strict';
 
-const lessFiles = {
-    'Client/css/site.css': 'node_modules/@osu-cass/smarter-balanced-styles/styles/site.less'
-};
-
-const lessProdFiles = {
-    'Client/css/about.css': 'Client/Styles/about.less',
-    'Client/css/home.css': 'Client/Styles/home.less',
-    'Client/css/item.css': 'Client/Styles/item.less',
-    'Client/css/nav.css': 'Client/Styles/nav.less',
-    'Client/css/search.css': 'Client/Styles/search.less',
-    'Client/css/site.css': 'Client/Styles/site.less'
-};
-
-
 module.exports = function (grunt) {
     grunt.initConfig({
      
-
-   
-
-        less: {
-            development: {
-                files: lessFiles,
-                sourceMap: true
-            },
-            production: {
-                files: lessFiles
-            }
-        },
-
-        
-
-     
-
         version: {
             package: {
                 src: ['package.json']
@@ -50,13 +19,9 @@ module.exports = function (grunt) {
                     'SmarterBalanced.SampleItems.Web.csproj'
                 ]
             }
-            
         }
     });
 
-    grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-version');
-
-
 
 };
