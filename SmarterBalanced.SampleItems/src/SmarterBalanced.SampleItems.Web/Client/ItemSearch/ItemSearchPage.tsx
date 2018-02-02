a71ae51f746f8357e82c7cae6b2642382438fffb
Brooks Mikkelsen <bmikkelsen22@gmail.com>
2018-02-02 12:30:20 -0800

    hide calc if math not selected before displaying advanced filters

--- SmarterBalanced.SampleItems/src/SmarterBalanced.SampleItems.Web/Client/ItemSearch/ItemSearchPage.tsx
+++ SmarterBalanced.SampleItems/src/SmarterBalanced.SampleItems.Web/Client/ItemSearch/ItemSearchPage.tsx
@@ -99,6 +99,7 @@ export class ItemsSearchComponent extends React.Component<Props, State> {
       let basicFilters = getBasicFilterCategories(itemSearchFilter, this.state.searchAPIParams);
       const searchModel = getItemSearchModel(itemSearchFilter);
       advancedFilters = Filter.getUpdatedSearchFilters(searchModel, advancedFilters, this.state.searchAPIParams);
+      advancedFilters = Filter.hideFiltersBasedOnSearchParams(advancedFilters, this.state.searchAPIParams);
       this.setState({
           itemSearch: { kind: "success", content: searchModel },
           advancedFilter: advancedFilters,
