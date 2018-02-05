8390d9d121cc983d5c1ecd4383ed782f961b25a0
Alex Lepinski <alexjlepinski@gmail.com>
2018-02-05 11:21:18 -0800

    changed home page layout to be within media width, added react modal overlay for screen readers

--- SmarterBalanced.SampleItems/src/SmarterBalanced.SampleItems.Web/Client/Home/Home.tsx
+++ SmarterBalanced.SampleItems/src/SmarterBalanced.SampleItems.Web/Client/Home/Home.tsx
@@ -14,36 +14,57 @@ export class Home extends React.Component<HomeProps, {}> {
     componentDidMount() {
         document.title = `Home - Smarter Balanced ${this.props.appName}`;
     }
+    renderWelcomeText() {
+        return (
+            <p> This site provides examples of test questions used on Smarter Balanced assessments in English language
+        arts / literacy and math.Teachers, parents, students, administrators, and policymakers can experience these test items just as students encounter them.</p>
+        );
+    }
+
+    renderAboutText() {
+        return (
+            <p>These samples are not intended to be used as practice tests, but educators can use them to better
+        understand how Smarter Balanced measures college- and career-ready content.</p>
+        );
+    }
+
+    renderHomeLinks() {
+        return (
+            <div className="home-links">
+                <NavLink to={'/BrowseItems'} exact activeClassName='active' className="btn-lg btn-primary home-container-button">
+                    <span>Browse Test Items</span>
+                </NavLink>
+                <NavLink to={'/AboutItems'} exact activeClassName='active' className="btn-lg btn-success home-container-button">
+                    <span>Learn More About Items</span>
+                </NavLink>
+
+            </div >
+        );
+    }
+    renderMain() {
+        return (
+            <div className="section home-welcome">
+                <h1 className="home-title"><b>Welcome!</b></h1>
+                <div className="home-content">
+                    {this.renderWelcomeText()}
+                    {this.renderAboutText()}
+                    {this.renderHomeLinks()}
+                </div>
+            </div >
+        );
+    }
 
     public render() {
-        return <div className="home-container">
-            <div className="container home-welcome">
-                <div>
-                    <h1 className="home-title"><b>Welcome!</b></h1>
-                    This site provides examples of test questions used on Smarter Balanced assessments in English language
-        arts/literacy and math. Teachers, parents, students, administrators, and policymakers can experience these test items just as students encounter them.
+        return (
+            <div className="home-page">
 
-        <br /><br />
+                <div className="home-container">
+                    <div className="container">
+                        {this.renderMain()}
+                    </div>
+                </div>
 
-                    These samples are not intended to be used as practice tests, but educators can use them to better
-        understand how Smarter Balanced measures college- and career-ready content.
-    </div>
-
-                <br />
-
-                <div className="text-center">
-                    <NavLink to={'/BrowseItems'} exact activeClassName='active' className="btn-lg btn-primary home-container-button">
-                        <span>Browse Test Items</span>
-                    </NavLink>
-
-
-                    <br /><br />
-                    <NavLink to={'/AboutItems'} exact activeClassName='active' className="btn-lg btn-success home-container-button">
-                        <span>Learn More About Items</span>
-                    </NavLink>
-
-                </div >
-            </div >
-        </div>;
+            </div>
+        );
     }
 }
