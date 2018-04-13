02a9cd741798ac49022b81c71183555c50a6efd2
Alex Lepinski <alexjlepinski@gmail.com>
2018-04-13 10:31:13 -0700

    uat fixes

--- SmarterBalanced.SampleItems/src/SmarterBalanced.SampleItems.Web/Client/ItemSearch/ItemSearch.ts
+++ SmarterBalanced.SampleItems/src/SmarterBalanced.SampleItems.Web/Client/ItemSearch/ItemSearch.ts
@@ -1,152 +1,131 @@
 ﻿import {
-  SearchAPIParamsModel,
-  AdvancedFilterCategoryModel,
-  ItemSearch,
-  ItemsSearchFilterModel,
-  ItemsSearchModel,
-  ItemCardModel,
-  getRequest,
-  GradeLevels,
-  OptionTypeModel,
-  BasicFilterCategoryModel,
-  FilterType
+    SearchAPIParamsModel,
+    AdvancedFilterCategoryModel,
+    ItemSearch,
+    ItemsSearchFilterModel,
+    ItemsSearchModel,
+    ItemCardModel,
+    getRequest,
+    GradeLevels,
+    OptionTypeModel,
+    BasicFilterCategoryModel,
+    FilterType
 } from "@osu-cass/sb-components";
 
 export const itemSearchClient = getRequest<ItemCardModel[]>(
-  "/BrowseItems/search"
+    "/BrowseItems/search"
 );
 
 export const itemsSearchFilterClient = getRequest<ItemsSearchFilterModel>(
-  "/BrowseItems/FilterSearchModel"
+    "/BrowseItems/FilterSearchModel"
 );
 
 export function getBasicFilterCategories(
-  itemSearchFilter: ItemsSearchFilterModel,
-  searchAPI: SearchAPIParamsModel
+    itemSearchFilter: ItemsSearchFilterModel,
+    searchAPI: SearchAPIParamsModel
 ): BasicFilterCategoryModel[] {
-  itemSearchFilter.grades.filterOptions = [
-    GradeLevels.Grade3,
-    GradeLevels.Grade4,
-    GradeLevels.Grade5,
-    GradeLevels.Grade6,
-    GradeLevels.Grade7,
-    GradeLevels.Grade8,
-    GradeLevels.High
-  ];
+    itemSearchFilter.grades.filterOptions = [
+        GradeLevels.Grade3,
+        GradeLevels.Grade4,
+        GradeLevels.Grade5,
+        GradeLevels.Grade6,
+        GradeLevels.Grade7,
+        GradeLevels.Grade8,
+        GradeLevels.High
+    ];
 
-  const grades: BasicFilterCategoryModel = {
-    ...ItemSearch.filterSearchToCategory(itemSearchFilter.grades, searchAPI),
-    optionType: OptionTypeModel.DropDown,
-    label: "Grade"
-  };
+    const grades: BasicFilterCategoryModel = {
+        ...ItemSearch.filterSearchToCategory(itemSearchFilter.grades, searchAPI),
+        optionType: OptionTypeModel.DropDown,
+        label: "Grade"
+    };
 
-  const subjects: BasicFilterCategoryModel = {
-    ...ItemSearch.filterSearchToCategory(itemSearchFilter.subjects, searchAPI),
-    optionType: OptionTypeModel.AdvFilter,
-    label: "Subject"
-  };
+    const subjects: BasicFilterCategoryModel = {
+        ...ItemSearch.filterSearchToCategory(itemSearchFilter.subjects, searchAPI),
+        optionType: OptionTypeModel.AdvFilter,
+        label: "Subject",
+        isMultiSelect: true
+    };
 
-  const claims: BasicFilterCategoryModel = {
-    ...ItemSearch.filterSearchToCategory(itemSearchFilter.claims, searchAPI),
-    optionType: OptionTypeModel.AdvFilter,
-    label: "Claims",
-    emptyOptionsText:"Select a Subject first."
-  };
+    const claims: BasicFilterCategoryModel = {
+        ...ItemSearch.filterSearchToCategory(itemSearchFilter.claims, searchAPI),
+        optionType: OptionTypeModel.AdvFilter,
+        label: "Claims",
+        emptyOptionsText: "Select a Subject first."
+    };
 
-  const targets: BasicFilterCategoryModel = {
-    ...ItemSearch.filterSearchToCategory(itemSearchFilter.targets, searchAPI),
-    filterOptions: [],
-    optionType: OptionTypeModel.AdvFilter,
-    label: "Targets",
-    emptyOptionsText: "Select a Subject and Claim first."
-  };
+    const targets: BasicFilterCategoryModel = {
+        ...ItemSearch.filterSearchToCategory(itemSearchFilter.targets, searchAPI),
+        filterOptions: [],
+        optionType: OptionTypeModel.AdvFilter,
+        label: "Targets",
+        emptyOptionsText: "Select a Subject and Claim first."
+    };
 
-  const searchItem: BasicFilterCategoryModel = {
-    disabled: false,
-    label: "Enter Item ID",
-    filterOptions: [],
-    helpText: "Item id helptext",
-    code: FilterType.SearchItemId,
-    optionType: OptionTypeModel.inputBox,
-    placeholderText: "Item ID \#"
-  };
+    const searchItem: BasicFilterCategoryModel = {
+        disabled: false,
+        label: "Enter Item ID",
+        filterOptions: [],
+        code: FilterType.SearchItemId,
+        optionType: OptionTypeModel.inputBox,
+        placeholderText: "Item ID \#"
+    };
 
-  return [grades, subjects, claims, targets, searchItem];
+    return [grades, subjects, claims, targets, searchItem];
 }
 
 export function getAdvancedFilterCategories(
-  itemSearchFilter: ItemsSearchFilterModel,
-  searchAPI: SearchAPIParamsModel
+    itemSearchFilter: ItemsSearchFilterModel,
+    searchAPI: SearchAPIParamsModel
 ): AdvancedFilterCategoryModel[] {
 
-  const claims = {
-    ...ItemSearch.filterSearchToCategory(itemSearchFilter.claims, searchAPI),
-    isMultiSelect: true,
-    disabled: false,
-    displayAllButton: true
-  };
 
-  const subjects = {
-    ...ItemSearch.filterSearchToCategory(itemSearchFilter.subjects, searchAPI),
-    isMultiSelect: true,
-    disabled: false,
-    displayAllButton: true
-  };
+    const interactions = {
+        ...ItemSearch.filterSearchToCategory(
+            itemSearchFilter.interactionTypes,
+            searchAPI
+        ),
+        isMultiSelect: true,
+        disabled: false,
+        displayAllButton: true
+    };
 
-  const interactions = {
-    ...ItemSearch.filterSearchToCategory(
-      itemSearchFilter.interactionTypes,
-      searchAPI
-    ),
-    isMultiSelect: true,
-    disabled: false,
-    displayAllButton: true
-  };
+    const techTypes: AdvancedFilterCategoryModel = {
+        ...ItemSearch.filterSearchToCategory(
+            itemSearchFilter.technologyTypes,
+            searchAPI
+        ),
+        isMultiSelect: false,
+        disabled: false,
+        displayAllButton: true
 
-  const techTypes = {
-    ...ItemSearch.filterSearchToCategory(
-      itemSearchFilter.technologyTypes,
-      searchAPI
-    ),
-    isMultiSelect: false,
-    disabled: false,
-    displayAllButton: true
-  };
+    };
 
-  const targets = {
-    ...ItemSearch.filterSearchToCategory(itemSearchFilter.targets, searchAPI),
-    isMultiSelect: true,
-    disabled: false,
-    displayAllButton: true
-  };
 
-  const calculator = {
-    ...ItemSearch.filterSearchToCategory(
-      itemSearchFilter.calculator,
-      searchAPI
-    ),
-    isMultiSelect: false,
-    disabled: false,
-    displayAllButton: true
-  };
+    const calculator: AdvancedFilterCategoryModel = {
+        ...ItemSearch.filterSearchToCategory(
+            itemSearchFilter.calculator,
+            searchAPI
+        ),
+        isMultiSelect: false,
+        disabled: false,
+        displayAllButton: true
+    };
 
-  return [
-    subjects,
-    claims,
-    interactions,
-    techTypes,
-    targets,
-    calculator
-  ];
+    return [
+        interactions,
+        techTypes,
+        calculator
+    ];
 }
 
 export function getItemSearchModel(
-  itemSearchFilter: ItemsSearchFilterModel
+    itemSearchFilter: ItemsSearchFilterModel
 ): ItemsSearchModel {
-  return {
-    claims: itemSearchFilter.claims.filterOptions,
-    subjects: itemSearchFilter.subjects.filterOptions,
-    interactionTypes: itemSearchFilter.interactionTypes.filterOptions,
-    targets: itemSearchFilter.targets.filterOptions
-  };
+    return {
+        claims: itemSearchFilter.claims.filterOptions,
+        subjects: itemSearchFilter.subjects.filterOptions,
+        interactionTypes: itemSearchFilter.interactionTypes.filterOptions,
+        targets: itemSearchFilter.targets.filterOptions
+    };
 }
